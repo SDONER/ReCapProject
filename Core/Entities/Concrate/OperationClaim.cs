9acf2573af7b5361888cab012da28f43f8f0544b
SDONER <serpil_4984@hotmail.com>
2021-03-09 21:31:10 +0300

    Updated with Json Web Token& Security Operations

---
+++ Core/Entities/Concrate/OperationClaim.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities.Concrate
+{
+    public class OperationClaim
+    {
+        public int Id { get; set; }
+        public string nAME { get; set; }
+    }
+}
---
+++ Core/Utilities/Security/Enycryption/SigningCredentalsHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Enycryption
+{
+    public class SigningCredentalsHelper
+    {
+        public static SigningCredentials CreateSigningCridentials(SecurityKey security)
+        {
+            return new SigningCredentials(security, SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
