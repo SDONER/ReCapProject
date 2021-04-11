f5721997ae16d17808464d2536838101630c71e4
SDONER <serpil_4984@hotmail.com>
2021-04-12 00:57:26 +0300

    ReCapProject Added Middleware

---
+++ Core/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Core.Extensions.Core.Extensions;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
